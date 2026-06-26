2d64357f7b4636f773c5c0d8b553ed7f9e915add
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-06-26 13:16:46 +0200

    feat: update projects section

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import Card from '../../components/card/card'
+import cardApi from "../../data/api.json"
 import "./projects.css"
 
-const projects = () => {
+const Projects = () => {
   return (
     <>
-        <section className="sectionProjects">
+        <section className="sectionProjects" id="projects">
             <h2>Mes Projets</h2>
+            <div className="cardsContainer">
+              {cardApi.map((card, index) => (
+                <Card
+                  key={index}
+                  image={card.image}
+                  title={card.title}
+                  link={card.link}
+                  tag={card.tag}
+              />
+              ))}
+            </div>
         </section>        
     </>
   )
 }
 
-export default projects
+export default Projects
