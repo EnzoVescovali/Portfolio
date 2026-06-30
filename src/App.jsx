b376b101c1a15a703e0182cadc8997874033546c
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-06-30 16:13:17 +0200

    feat: update app component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Footer from './components/footer/footer'
 import Hero from './sections/hero/hero'
 import Projects from './sections/projects/projects'
 import Contact from './sections/contact/contact'
+import Toaster from './components/toaster/toaster'
 
 const App = () => {
   return (
     <>
+      <Toaster/>
       <Navbar/>
       <Hero/>
       <Projects/>
