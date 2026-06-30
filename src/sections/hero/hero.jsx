b124b68616d0862ca12aace98b80f1f451a19fb8
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-06-30 16:13:19 +0200

    style: update hero section

--- src/sections/hero/hero.jsx
+++ src/sections/hero/hero.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextHero from '../../components/textHero/textHero'
+import Modal from '../../components/modal/modal'
 import './hero.css'
 import '../../styles/button.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,6 +8,7 @@ import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
 const hero = () => {
+  const [showModal, setShowModal] = useState(false)
   return (
     <>
         <section className="sectionHero" id="hero">
@@ -17,18 +19,7 @@ const hero = () => {
               Explorez mes projets et découvrez mon approche du développement web en tant que développeur junior, entre design, performance et souci du détail.
           </p>
           <div className="buttonsContainer">
-              <a href="" className='btn btnPrimary'>
-                <span className="btnContent btnDefault">
-                  Voir mon CV
-                  <FontAwesomeIcon icon={faEye} size="xl" className='heroIcon'/>
-                </span>
-                <span className="btnContent btnHover" aria-hidden="true">
-                  Voir mon CV
-                  <FontAwesomeIcon icon={faEye} size="xl" className='heroIcon'/>
-                </span>
-              </a>
-
-              <a href="" download className='btn btnSecondary'>
+              <a href="../../public/CV_Enzo_Vescovali.pdf" download className='btn btnSecondary'>
                 <span className="btnContent btnDefault">
                   Telecharger mon CV 
                   <FontAwesomeIcon icon={faDownload} size="xl" className='heroIcon'/>
@@ -38,8 +29,19 @@ const hero = () => {
                   <FontAwesomeIcon icon={faDownload} size="xl" className='heroIcon'/>
                 </span>
               </a>
+              <button onClick={() => setShowModal(true)} className='btn btnPrimary'>
+                <span className="btnContent btnDefault">
+                  Voir mon CV
+                  <FontAwesomeIcon icon={faEye} size="xl" className='heroIcon'/>
+                </span>
+                <span className="btnContent btnHover" aria-hidden="true">
+                  Voir mon CV
+                  <FontAwesomeIcon icon={faEye} size="xl" className='heroIcon'/>
+                </span>
+              </button>
           </div>
         </section>
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)} />
     </>
   )
 }
