7f36751a0f1d20b91a18780d1d3bae84ed304ba7
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-07-03 16:12:56 +0200

    update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import "./navbar.css"
+import React, { useState } from 'react'
+import './navbar.css'
 import { NavLink } from 'react-router-dom'
 
-const navbar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const closeMenu = () => setIsOpen(false)
+
   return (
-    <>
-        <nav>
-            <NavLink to="/" className="logo">
-                Vipe
-            </NavLink>
-            <ul>
-                <li>
-                    <a href="#hero">Accueil</a>
-                </li>
-                <li>
-                    <a href="#projects">Projets</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </nav>
-    </>
+    <nav className="navbar">
+      <NavLink to="/" className="logo" onClick={closeMenu}>
+        Vipe
+      </NavLink>
+
+      <button
+        type="button"
+        className={`menu-toggle ${isOpen ? 'active' : ''}`}
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label={isOpen ? 'Fermer le menu' : 'Ouvrir le menu'}
+        aria-expanded={isOpen}
+        aria-controls="navbar-menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <ul id="navbar-menu" className={`nav-links ${isOpen ? 'open' : ''}`}>
+        <li>
+          <a href="#hero" onClick={closeMenu}>Accueil</a>
+        </li>
+        <li>
+          <a href="#projects" onClick={closeMenu}>Projets</a>
+        </li>
+        <li>
+          <a href="#contact" onClick={closeMenu}>Contact</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar
