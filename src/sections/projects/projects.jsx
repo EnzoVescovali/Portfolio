14c9771a901f33a8213b653c93fc5b4e0f28eefe
EnzoVescovali <e.vescovali.ev@gmail.com>
2026-07-10 00:57:38 +0200

    feat: expand the projects section

--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -1,27 +1,53 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import Card from '../../components/card/card'
-import cardApi from "../../data/api.json"
-import "./projects.css"
+import cardApi from '../../data/api.json'
+import './projects.css'
+import ProjectDetailsModal from '../../components/projectDetailsModal/projectDetailsModal'
 
 const Projects = () => {
+  const [selectedProject, setSelectedProject] = useState(null)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const projects = useMemo(() => cardApi || [], [])
+
+  const openModal = (project) => {
+    setSelectedProject(project)
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setSelectedProject(null)
+  }
+
   return (
     <>
-        <section className="sectionProjects" id="projects">
-            <h2>Mes Projets</h2>
-            <div className="cardsContainer">
-              {cardApi.map((card, index) => (
-                <Card
-                  key={index}
-                  image={card.image}
-                  title={card.title}
-                  link={card.link}
-                  tag={card.tag}
-              />
-              ))}
-            </div>
-        </section>        
+      <ProjectDetailsModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        project={selectedProject}
+      />
+
+      <section className="sectionProjects" id="projects">
+        <h2>Mes Projets</h2>
+
+        <div className="cardsContainer">
+          {projects.map((project, index) => (
+            <Card
+              key={project.id ?? index}
+              project={project}
+              image={project.image}
+              title={project.title}
+              link={project.github || project.link}
+              tag={project.tag || project.stack || []}
+              onOpen={openModal}
+            />
+          ))}
+        </div>
+      </section>
     </>
   )
 }
 
 export default Projects
+
